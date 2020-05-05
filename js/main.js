f2f6f0411f0f4c7ca0ffafc8386e222ee0bd373c
Marina Muratova <frau.muratova@gmail.com>
2020-05-05 21:41:17 +0300

    tasks for day1 done

--- js/main.js
+++ js/main.js
@@ -9,3 +9,71 @@ function toggleModal() {
   modal.classList.toggle("is-open");
 }
 
+//день 1
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+const loginInput = document.querySelector("#login");
+const userName = document.querySelector(".user-name");
+const buttonOut = document.querySelector(".button-out");
+
+let login = localStorage.getItem("marina");
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle("is-open");
+}
+
+function authorized() {
+  function logOut() {
+    login = null;
+    localStorage.removeItem("marina");
+    buttonAuth.style.display = "";
+    userName.style.display = "";
+    buttonOut.style.display = "";
+    buttonOut.removeEventListener("click", logOut);
+
+    checkAuth();
+  }
+
+  console.log("Авторизован");
+  userName.textContent = login;
+  buttonAuth.style.display = "none";
+  userName.style.display = "inline";
+  buttonOut.style.display = "block";
+  buttonOut.addEventListener("click", logOut);
+}
+
+function notAuthorized() {
+  console.log("Не авторизован");
+
+  function logIn(event) {
+    event.preventDefault();
+    if (loginInput.value.length == 0) {
+      alert("Введите логин");
+    }
+
+    login = loginInput.value;
+
+    localStorage.setItem("marina", login);
+    toggleModalAuth();
+    buttonAuth.removeEventListener("click", toggleModalAuth);
+    closeAuth.removeEventListener("click", toggleModalAuth);
+    logInForm.removeEventListener("submit", logIn);
+    logInForm.reset();
+    checkAuth();
+  }
+
+  buttonAuth.addEventListener("click", toggleModalAuth);
+  closeAuth.addEventListener("click", toggleModalAuth);
+  logInForm.addEventListener("submit", logIn);
+}
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+checkAuth();
